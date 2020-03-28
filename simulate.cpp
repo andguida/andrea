d116576ffa0b4187d7261ee6fad1f8d05db0dc6a
andguida <61746669+andguida@users.noreply.github.com>
2020-03-28 11:21:27 +0100

    Add files via upload

--- simulate.cpp
+++ simulate.cpp
@@ -2,12 +2,16 @@
 // Created by 15au116nl on 27/03/2020.
 //
 
-#include "simulate.h"
-#include "iostream"
 
-void simulate(){
 
-    return 0
+
+void simulate(double a, double b, double x, double u, double dt){
+    double dx ;
+    dx = a*x + b*u;
+    x += dt*dx;
+
+cout << x;
+
 
 
 }
